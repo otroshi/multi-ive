eafb4ce7327b1413cc5e5f6647ae04bdd14e263f
PietroMelzi <pietromelzi95@gmail.com>
2022-07-13 15:59:24 +0200

    verification evaluation and training of IVE with all the samples

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import evaluation.utils as eval_utils
 import IVE.utils as ive_util
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 
 # create
@@ -15,6 +16,11 @@ if not os.path.isfile('data/train_df.csv'):
 else:
 	train_df = pd.read_csv('data/train_df.csv')
 	test_df = pd.read_csv('data/test_df.csv')
+
+total_indexes = [i for i in range(len(train_df) + len(test_df))]
+total_df = pd.concat([train_df, test_df], sort=False)
+total_df['indexes'] = total_indexes
+total_df = total_df.set_index('indexes')
 
 # set useful variables
 labels = ['sex', 'age', 'ethnicity']
@@ -32,61 +38,57 @@ n_e = [int(num_eliminations / 3), int(num_eliminations / 3), int(num_elimination
 elimination_order = [0, 1, 2]
 
 # get embeddings
-x_train = manage_data.get_x_ready(train_df, length_embeddings)
-x_test = manage_data.get_x_ready(test_df, length_embeddings)
+x_total = manage_data.get_x_ready(total_df, length_embeddings)
 
 # normalize embeddings
 std_sc = StandardScaler()
-x_train = std_sc.fit_transform(x_train)
-x_test = std_sc.transform(x_test)
+# fit the scaler with only training data
+std_sc.fit(x_total[:len(train_df)])
+x_total = std_sc.transform(x_total)
 
 # get labels
-y_train = manage_data.get_y_ready(train_df, labels)
-y_test = manage_data.get_y_ready(test_df, labels)
+y = manage_data.get_y_ready(total_df, labels)
 
-x_train_first = x_train
-x_train_second = x_train
-x_train_third = x_train
-x_test_first = x_test
-x_test_second = x_test
-x_test_third = x_test
+x_first = np.copy(x_total)
+x_second = np.copy(x_total)
+x_third = np.copy(x_total)
 
 # helper for the evaluation of verification performance
-verification_indexes = vp.get_indexes(test_df)
+verification_indexes = vp.get_indexes(total_df)
 # to keep track of all the evaluation metrics computed
 metrics = eval_utils.get_metric_dict()
 
 for epoch in range(num_epochs):
 	print('--- Epoch {} ---'.format(epoch))
-	print('First size: {} - Second size: {} - Third size: {}'.format(x_train_first.shape[1],
-																	 x_train_second.shape[1], x_train_third.shape[1]))
-	for ive_method, x in enumerate(zip([x_train_first, x_train_second, x_train_third], [x_test_first, x_test_second, x_test_third])):
-		x_train = x[0]
-		x_test = x[1]
+	print('First size: {} - Second size: {} - Third size: {}'.format(x_first.shape[1], x_second.shape[1], x_third.shape[1]))
+	for ive_method, x in enumerate([x_first, x_second, x_third]):
+		# train and test sets for the evaluation of soft biometrics
+		x_train = x[:len(train_df)]
+		y_train = y[:len(train_df)]
+		x_test = x[len(train_df):]
+		y_test = y[len(train_df):]
+
 		scores = []
 		for i, label in enumerate(labels):
 			# compute the scores of every sb-classifier and store them
 			score = simple_classifier.train_evaluate_simple_classifier(x_train, y_train[:, i], x_test, y_test[:, i])
 			scores.append(score)
-
 		print(scores)
-		eer_verification = vp.evaluate_verification(verification_indexes, x_test)
+
+		eer_verification = vp.evaluate_verification(verification_indexes, x)
 		print(eer_verification)
 		metrics = eval_utils.store_metrics(metrics, ive_method, scores, eer_verification)
 
-	first_ive = ive_util.first_method(x_train_first, y_train, model_train, n_e, n_s, elimination_order)
+	first_ive = ive_util.first_method(x_first, y, model_train, n_e, n_s, elimination_order)
 	# print('first ive training done')
-	second_ive = ive_util.second_method(x_train_second, y_train, model_train, num_eliminations, num_steps)
+	second_ive = ive_util.second_method(x_second, y, model_train, num_eliminations, num_steps)
 	# print('second ive training done')
-	third_ive = ive_util.third_method(x_train_third, y_train, model_train, num_eliminations, num_steps)
+	third_ive = ive_util.third_method(x_third, y, model_train, num_eliminations, num_steps)
 	# print('third ive training done')
 
-	x_train_first = first_ive[2].transform(first_ive[1].transform(first_ive[0].transform(x_train_first)))
-	x_train_second = second_ive.transform(x_train_second)
-	x_train_third = third_ive.transform(x_train_third)
-	x_test_first = first_ive[2].transform(first_ive[1].transform(first_ive[0].transform(x_test_first)))
-	x_test_second = second_ive.transform(x_test_second)
-	x_test_third = third_ive.transform(x_test_third)
+	x_first = first_ive[2].transform(first_ive[1].transform(first_ive[0].transform(x_first)))
+	x_second = second_ive.transform(x_second)
+	x_third = third_ive.transform(x_third)
 
 eval_utils.plot_metrics(metrics)
 
